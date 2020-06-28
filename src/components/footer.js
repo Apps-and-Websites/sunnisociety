486db20a51861921cd4c34b895cf2f1d99ab5935
Mohd Raza <noorinfotechinc@gmail.com>
2020-06-27 21:57:04 -0500

    mission statement in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function footer() {
   return (
     <footer className="footer">
-      <p> All rights reserved</p>
+      <p>
+        {" "}
+        The mission of Sunni Society is to promote Islamic Scholarly teachings
+        under the guidance of Shaykh-ul-Hadis Mufti Ahmadul Qaderi Founder and
+        Imam-o-Khateeb of Noori Masjid, Plano TX
+      </p>
     </footer>
   );
 }
