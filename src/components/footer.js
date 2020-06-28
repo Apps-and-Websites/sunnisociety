c54c0784af6147fadfc7868a5b067f1ed1b814cf
Mohd Raza <noorinfotechinc@gmail.com>
2020-06-27 22:01:44 -0500

    footer paragraph styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,6 @@ export default function footer() {
   return (
     <footer className="footer">
       <p>
-        {" "}
         The mission of Sunni Society is to promote Islamic Scholarly teachings
         under the guidance of Shaykh-ul-Hadis Mufti Ahmadul Qaderi Founder and
         Imam-o-Khateeb of Noori Masjid, Plano TX
